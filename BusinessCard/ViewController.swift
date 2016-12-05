9a238127d7c63d2a4fc72fc185efe963d572d7d7
pressstart101 <walterscott404@yandex.com>
2016-12-04 22:30:36 -0500

    Initial Commit

---
+++ BusinessCard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessCard
+//
+//  Created by walter scott on 12/4/16.
+//  Copyright © 2016 mk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
